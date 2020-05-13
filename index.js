edfd3df55455cceb598d0f05f5d0c901dd8a9bac
Ting Chung <tchung682@gmail.com>
2020-05-13 15:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 
 // Add your changeCompletely() function here:
 
-var evens = [0, 2, 4, 6, 8, 10];
-
-evens.forEach((even, index, array) => {
-  debugger;
-  console.log(`${even} is not odd!`);
-});
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
+ 
+var animals = ["dog", "cat", "squirrel"];
+ 
+doToElementsInArray(animals, changeCompletely);
+ 
+// log out animals -- pretty cool, right?
+console.log(animals);
