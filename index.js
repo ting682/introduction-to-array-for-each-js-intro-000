974336986fe06b06a6080b5aea1e078ec120705a
Ting Chung <tchung682@gmail.com>
2020-05-13 14:51:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function doToElementsInArray(array, callback) {
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
- 
+
 var animals = ["dog", "cat", "squirrel"];
- 
+
 doToElementsInArray(animals, changeCompletely);
- 
+
 // log out animals -- pretty cool, right?
 console.log(animals);
