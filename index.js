42d6e57d9b93a42e8ba25f69e01badf1b3d8ab06
Ting Chung <tchung682@gmail.com>
2020-05-13 15:04:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 
 
 // Add your changeCompletely() function here:
-var evens = [0, 2, 4, 6, 8, 10];
 
+var evens = [0, 2, 4, 6, 8, 10];
+ 
 evens.forEach((even, index, array) => {
   debugger;
   console.log(`${even} is not odd!`);
